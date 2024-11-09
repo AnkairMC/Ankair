b1eb9dbc702f6d94181b4c16671a0d6f6b8b36da
xuyin <1573880184@qq.com>
2024-11-09 22:59:52 +0800

    Slightly modified 'xyz.ankairmc.ankair.network.LegacyPingHandler'

--- src/main/java/xyz/ankairmc/ankair/network/LegacyPingHandler.java
+++ src/main/java/xyz/ankairmc/ankair/network/LegacyPingHandler.java
@@ -4,42 +4,45 @@ import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.SimpleChannelInboundHandler;
 import xyz.ankairmc.ankair.MinecraftServer;
 import xyz.ankairmc.ankair.server.MinecraftStatus;
 
 import java.nio.charset.StandardCharsets;
 
-public class LegacyPingHandler extends ChannelInboundHandlerAdapter {
+public class LegacyPingHandler extends SimpleChannelInboundHandler<ByteBuf> {
+    public LegacyPingHandler() {
+        super(false);
+    }
+
     @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+    protected void channelRead0(ChannelHandlerContext ctx, ByteBuf input) {
         MinecraftStatus status = MinecraftServer.getServer();
-        if (msg instanceof ByteBuf buf) {
-            int id = buf.readUnsignedByte();
+        int id = input.readUnsignedByte();
 
-            try {
-                if (id == 0xFE) {
-                    ByteBuf bytebuf = ctx.alloc().buffer();
-                    String response = String.format(
-                            "\u00a71\u0000%d\u0000%s\u0000%s\u0000%d\u0000%d",
-                            status.getProtocol(),
-                            status.getVersionName(),
-                            status.getMotd(),
-                            status.getOnline(),
-                            status.getMax()
-                    );
+        try {
+            if (id == 0xFE) {
+                ByteBuf bytebuf = ctx.alloc().buffer();
+                String response = String.format(
+                        "\u00a71\u0000%d\u0000%s\u0000%s\u0000%d\u0000%d",
+                        status.getProtocol(),
+                        status.getVersionName(),
+                        status.getMotd(),
+                        status.getOnline(),
+                        status.getMax()
+                );
 
-                    bytebuf.writeByte(255);
-                    bytebuf.writeShort(response.length());
-                    bytebuf.writeCharSequence(response, StandardCharsets.UTF_16BE);
+                bytebuf.writeByte(255);
+                bytebuf.writeShort(response.length());
+                bytebuf.writeCharSequence(response, StandardCharsets.UTF_16BE);
 
-                    ctx.writeAndFlush(bytebuf).addListener(ChannelFutureListener.CLOSE);
-                }
-            } finally {
-                buf.resetReaderIndex();
+                ctx.writeAndFlush(bytebuf).addListener(ChannelFutureListener.CLOSE);
             }
-
-            ctx.channel().pipeline().remove(this);
-            ctx.fireChannelRead(msg);
+        } finally {
+            input.resetReaderIndex();
         }
+
+        ctx.channel().pipeline().remove(this);
+        ctx.fireChannelRead(input);
     }
 }
